c2a57bca90e0eccd51104522a1d18db53109600e
alexxndrin <alexandrinnnnna@gmail.com>
2026-06-15 15:14:56 +0300

    Update server.js
    v2

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.use((req, res) => {
-    res.status(404).send('<h1>Ошибка 404: Страница не найдена</h1>');
+app.get('*', (req, res) => {
+    const filePath = path.join(__dirname, 'public', req.path);
+    const fs = require('fs');
+    
+    if (path.extname(req.path)) {
+        if (fs.existsSync(filePath)) {
+            res.sendFile(filePath);
+        } else {
+            res.status(404).send('<h1>Ошибка 404: Страница не найдена</h1>');
+        }
+    } else {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
 });
 
 app.listen(port, () => {
